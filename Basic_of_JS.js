b56b2bd2ab38746730bda8571f9e534c711aa70d
Sayali-queen03 <shelarsayali385@gmail.com>
2024-12-20 19:44:47 +0530

    Add files via upload

---
+++ Basic_of_JS.js
@@ -0,0 +1,136 @@
+// printing in JS
+console.log("Hi I am a Coder");
+console.log("Hello,Everyone");
+console.log('What are doing');
+
+// Variables
+var a=5;      //declare
+var a=1000;       //allow redeclare and reassign 
+console.log(a);
+
+let myName="Sayali Shelar"  //declare
+myName="Queen03"            //allow only reassign not redeclare
+console.log(myName);
+
+const pi=3.14;     
+console.log(pi);        //don't allow both reassign and redeclare
+
+// Datatypes
+let A=768;
+let b=45.76;
+let c=-89
+console.log(typeof(A));            // number
+console.log(typeof(b));
+console.log(typeof(c));
+
+let city="karad";
+let state='maharashtra'
+let num="98478963948"               
+console.log(typeof(city));              //string
+console.log(typeof(state));
+console.log(typeof(num));
+
+let isCollege=true;
+let isflower=false;                       //Boolean
+console.log(typeof(isCollege));
+console.log(typeof(isflower));
+
+let n;
+console.log(typeof(n));          //undefind
+
+let q=null;
+console.log(typeof(q));        //null
+
+let x=BigInt("123")
+console.log(x);                     //Bigint
+console.log(typeof(x));
+             
+
+let p=Symbol("Hello")
+console.log(p);                //symbol
+console.log(typeof(p));
+
+
+//  Operator
+// 1) Arithmetic 
+let i=8;
+let j=4;
+console.log(i+j);
+console.log(i-j);
+console.log(i*j);
+console.log(i/j);
+console.log(i%j);
+console.log(i**j);
+
+// 2)Logical
+let m=10;
+let o=20;
+console.log(m && o);
+console.log(m || o);
+console.log(!(m && o));
+
+// 3)Assignment
+let s=50;
+let z=5;
+console.log(s+=z);
+console.log(s-=z);
+console.log(s*=z);
+console.log(s/=z);
+console.log(s%=z);
+
+// 4)Comparison
+let num1=55;
+let num2=60;
+console.log(num1==num2);
+console.log(num1!=num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1>num2);
+console.log(num1<num2);
+
+let num3=123;
+let num4="123";
+console.log(num3===num4);
+console.log(num3!==num4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i=20;i>0;i--){
+//     console.log(i);
+    
+// }
+
+// sum=0
+// for(let i=1;i<=100;i++){
+//     if(i%2==0){
+//         sum+=i
+//         }
+//     }console.log(sum);
+
+
+    
