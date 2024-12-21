7a6e860704e4d8fd12fc11dd1cfb1895442b79d2
Sayali-queen03 <shelarsayali385@gmail.com>
2024-12-21 18:05:53 +0530

    Add files via upload

--- Basic_program.js
+++ Basic_program.js
@@ -94,3 +94,15 @@ if (tempCelsius < 0) {
 } else {
     console.log("It's hot.");
 }
+
+
+
+
+let char = 'E';
+
+if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U' ||
+    char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+    console.log(char + " is a vowel.");
+} else {
+    console.log(char + " is a consonant.");
+}
